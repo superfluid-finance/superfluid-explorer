f84f5634cd1009049b2f5ca832a389223eff8359
Kaspar Kallas <kaspar@superfluid.finance>
2022-02-04 16:05:15 +0200

    Clean up index subscription page/dialog

--- src/components/IndexSubscriptionDetails.tsx
+++ src/components/IndexSubscriptionDetails.tsx
@@ -1,111 +1,13 @@
-import {FC, useState} from "react";
-import {sfSubgraph} from "../redux/store";
-import {
-  createSkipPaging, Index, IndexSubscription,
-  Ordering,
-  SkipPaging, SubscriptionUnitsUpdatedEvent_OrderBy
-} from "@superfluid-finance/sdk-core";
-import Container from "@mui/material/Container";
+import { FC, useState } from "react";
 import {
   Box,
-  Button,
-  Card,
-  List,
-  ListItem, ListItemText, Skeleton,
-  Typography
+  Button
 } from "@mui/material";
-import SubscriptionUnitsUpdatedEventDataGrid from "./SubscriptionUnitsUpdatedEventDataGrid";
-import {BigNumber} from "ethers";
-import {skipToken} from "@reduxjs/toolkit/query";
 import DetailsDialog from "./DetailsDialog";
-import SuperTokenAddress from "./SuperTokenAddress";
-import AccountAddress from "./AccountAddress";
-import SkeletonAddress from "./skeletons/SkeletonAddress";
-import {Network} from "../redux/networks";
+import { Network } from "../redux/networks";
+import { IndexSubscriptionPageContent } from "../pages/[networkName]/index-subscriptions/[id]";
 
-interface Props {
-  network: Network;
-  indexSubscriptionId: string
-}
-
-const IndexSubscriptionDetails: FC<Props> = ({network, indexSubscriptionId}) => {
-  const indexSubscriptionQuery = sfSubgraph.useIndexSubscriptionQuery({
-    chainId: network.chainId,
-    id: indexSubscriptionId
-  });
-
-  const indexSubscription: IndexSubscription | undefined | null = indexSubscriptionQuery.data
-
-  const indexQuery = sfSubgraph.useIndexQuery(indexSubscription ? {
-    chainId: network.chainId,
-    id: indexSubscription.index
-  } : skipToken);
-
-  const index: Index | undefined | null = indexQuery.data
-
-  const [subscriptionUnitsUpdatedEventPaging, setSubscriptionUnitsUpdatedEventPaging] = useState<SkipPaging>(createSkipPaging({
-    take: 10
-  }))
-  const [subscriptionUnitsUpdatedEventPagingOrdering, setSubscriptionUnitsUpdatedEventOrdering] = useState<Ordering<SubscriptionUnitsUpdatedEvent_OrderBy> | undefined>()
-  const subscriptionUnitsUpdatedEventQuery = sfSubgraph.useSubscriptionUnitsUpdatedEventsQuery({
-    chainId: network.chainId,
-    filter: {
-      subscription: indexSubscriptionId.toLowerCase()
-    },
-    pagination: subscriptionUnitsUpdatedEventPaging,
-    order: subscriptionUnitsUpdatedEventPagingOrdering
-  });
-
-  return (<Container>
-    <Typography variant="h2">
-      Index Subscription Details
-    </Typography>
-    <Card variant="outlined">
-      <List>
-        <ListItem divider>
-          <ListItemText secondary="Token"
-                        primary={(network && indexSubscription) ?
-                          <SuperTokenAddress network={network} address={indexSubscription.token}/> :
-                          <SkeletonAddress/>}/>
-        </ListItem>
-        <ListItem divider>
-          <ListItemText secondary="Publisher"
-                        primary={(network && indexSubscription) ?
-                          <AccountAddress network={network} address={indexSubscription.publisher}/> :
-                          <SkeletonAddress/>}/>
-        </ListItem>
-        <ListItem divider>
-          <ListItemText secondary="Subscriber"
-                        primary={(network && indexSubscription) ?
-                          <AccountAddress network={network} address={indexSubscription.subscriber}/> :
-                          <SkeletonAddress/>}/>
-        </ListItem>
-        <ListItem>
-          <ListItemText secondary="Total Units Received" primary={(indexSubscription && index) ? calculateUnitsReceived(
-            BigNumber.from(index.indexValue),
-            BigNumber.from(indexSubscription.totalAmountReceivedUntilUpdatedAt),
-            BigNumber.from(indexSubscription.indexValueUntilUpdatedAt),
-            Number(indexSubscription.units)
-          ).toString() : <Skeleton sx={{width: "100px"}}/>}/>
-        </ListItem>
-      </List>
-    </Card>
-    <Card variant="outlined">
-      <Typography variant="h3">
-        Distributions
-      </Typography>
-      <SubscriptionUnitsUpdatedEventDataGrid
-        queryResult={subscriptionUnitsUpdatedEventQuery}
-        setPaging={setSubscriptionUnitsUpdatedEventPaging}
-        ordering={subscriptionUnitsUpdatedEventPagingOrdering}
-        setOrdering={setSubscriptionUnitsUpdatedEventOrdering}/>
-    </Card>
-  </Container>)
-};
-
-export default IndexSubscriptionDetails;
-
-export const IndexSubscriptionDetailsDialog: FC<Props> = (props) => {
+export const IndexSubscriptionDetailsDialog: FC<{ indexSubscriptionId: string, network: Network }> = (props) => {
   const [open, setOpen] = useState(false);
 
   const handleClickOpen = () => {
@@ -122,23 +24,8 @@ export const IndexSubscriptionDetailsDialog: FC<Props> = (props) => {
         Details
       </Button>
       <DetailsDialog open={open} handleClose={handleClose}>
-        <IndexSubscriptionDetails {...props} />
+        <IndexSubscriptionPageContent {...props} />
       </DetailsDialog>
     </Box>
   );
 }
-
-const calculateUnitsReceived = (
-  publisherIndexValue: BigNumber,
-  subscriberTotalAmountReceivedUntilUpdatedAt: BigNumber,
-  subscriberIndexValueUntilUpdatedAt: BigNumber,
-  subscriberUnits: number
-) => {
-  const totalUnitsReceived = subscriberTotalAmountReceivedUntilUpdatedAt.add(
-    publisherIndexValue
-      .sub(subscriberIndexValueUntilUpdatedAt)
-      .mul(subscriberUnits)
-  );
-
-  return totalUnitsReceived;
-};
